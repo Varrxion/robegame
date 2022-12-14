f2e6d8feab2d577e3135372f2e01106c87d550b8
Hartsej <64440637+Keklord-Channel4@users.noreply.github.com>
2022-12-14 12:46:56 -0500

    Initial Commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robe
+{
+	public class Robegame
+	{
+		static void Main()
+		{
+			Console.WriteLine("TEST");
+		}
+	}
+}
---
+++ player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robe
+{
+    internal class Player
+    {
+        private int Health { get; set; }
+        private int APower { get; set; }
+    }
+}
