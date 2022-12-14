9973f9041e4c8d4d311c98c8f63eaf0efafc3a13
TheUserEnds <106560910+TheUserEnds@users.noreply.github.com>
2022-12-14 14:09:57 -0500

    History files for Graphics.cs
    History files for Graphics.cs

---
+++ .history/Graphics_20221214134237.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robe
+{
+    internal class Graphics
+    {
+        public void DrawTitle()
+        {
+            console.WriteLine(" ______________________________________________________  ");
+            console.WriteLine("/_____/_____/_____/_____/_____/_____/_____/_____/_____/  ");
+            console.WriteLine(" ___    __________ ________ _____________________   ___   ");
+            console.WriteLine(" \  \   \______   \\_____  \\______   \_   _____/   \  \  ");
+            console.WriteLine("  \  \   |      __/ /  | |  \|       _/|    __)_     \  \ ");
+            console.WriteLine("   |  )  |    |  \ /   | |   \    |   \|        \     |  ) ");
+            console.WriteLine("  /__/   |____| \_\\_______  /____|_  /_______  /    /__/ ");
+            console.WriteLine(" /__/        \/            \/       \/        \/    /__/");
+            console.WriteLine(" ______________________________________________________  ");
+            console.WriteLine("/_____/_____/_____/_____/_____/_____/_____/_____/_____/  ");
+            
+        }
+    }
+}
---
+++ .history/Graphics_20221214135213.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robe
+{
+    internal class Graphics
+    {
+        public void DrawTitle()
+        {
+            console.WriteLine(" ______________________________________________________");
+            console.WriteLine("/_____/_____/_____/_____/_____/_____/_____/_____/_____/");
+            console.WriteLine(" ___    ________________________________________   ___");
+            console.WriteLine(" \  \   \______  \   / ___\  |   ___ \|   _____/   \  \");
+            console.WriteLine("  \  \   |      _/  /  | | \ |   |_|_/|   ____/     \  \");
+            console.WriteLine("   |  )  |    |  \ /   |_|  \|   |_| \|       \      |  )");
+            console.WriteLine("  /__/   |____| \_\\_______ /|______  /_______ \    /__/");
+            console.WriteLine(" /__/        \/           \/        \/        \/   /__/");
+            console.WriteLine(" ______________________________________________________");
+            console.WriteLine("/_____/_____/_____/_____/_____/_____/_____/_____/_____/");
+
+        }
+    }
+}
