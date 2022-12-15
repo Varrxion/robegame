b85e54468206094c66dd8b6836492391c76054f8
Hartsej <jordan3827@gmail.com>
2022-12-14 19:59:11 -0500

    Change relics to collection

--- Inventory.cs
+++ Inventory.cs
@@ -9,19 +9,13 @@ namespace Robe
     internal static class Inventory
     {
         private static int Gold = 0;
-        private static bool FireRelic = false; //Increases damage dealt by 50%
-        private static bool FrostRelic = false; //Decreases damage taken by 50%
+        private static bool[] relics = new bool[2];
 
         //Gold
         public static int GetGold() { return Gold; }
         public static void SetGold(int gold) { Gold = gold; }
         //Fire Relic
-        public static bool GetFireRelic() { return FireRelic; }
-        public static void SetFireRelic(bool has) { FireRelic = has; }
-        //Frost Relic
-        public static bool GetFrostRelic() { return FrostRelic; }
-        public static void SetFrostRelic(bool has) { FrostRelic = has; }
-
-
+        public static bool GetRelic(int index) { return relics[index]; }
+        public static void SetRelic(int index, bool has) { relics[index] = has; }
     }
 }
